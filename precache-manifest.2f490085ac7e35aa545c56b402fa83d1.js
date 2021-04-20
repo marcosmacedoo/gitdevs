f8f70d0f1c819b936105192453edffe49be61aa5
marcosmacedoo <marcosv2468@gmail.com>
2021-04-20 10:16:19 -0300

    Updates

---
+++ precache-manifest.2f490085ac7e35aa545c56b402fa83d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55e686a500a3be189362bedf514f8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc72eb7bd45d42d9bc2",
+    "url": "/static/css/main.5638960e.chunk.css"
+  },
+  {
+    "revision": "26426f91f9a87ba56a25",
+    "url": "/static/js/2.6f88340d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f88340d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc72eb7bd45d42d9bc2",
+    "url": "/static/js/main.c294d9ac.chunk.js"
+  },
+  {
+    "revision": "3b193c3944e25033f9be",
+    "url": "/static/js/runtime-main.cb31a61f.js"
+  },
+  {
+    "revision": "89e3ceb0ea9dbc776a42d1e863008ffe",
+    "url": "/static/media/logo-github.89e3ceb0.svg"
+  }
+]);
